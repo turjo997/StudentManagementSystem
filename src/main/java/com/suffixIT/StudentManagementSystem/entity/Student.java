34807b8a9d3bbe41228e5ae8f575364e3a94022d
mohibul361 <mohibul323@gmail.com>
2023-10-02 16:28:56 +0600

    Student is modified

--- src/main/java/com/suffixIT/StudentManagementSystem/entity/Student.java
+++ src/main/java/com/suffixIT/StudentManagementSystem/entity/Student.java
@@ -1,4 +1,26 @@
 package com.suffixIT.StudentManagementSystem.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "student")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer studentId;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String studentAddress;
+
+
 }
